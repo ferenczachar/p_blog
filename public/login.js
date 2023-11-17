1c06ce569206c545fff8fe36f5f4f2cb33bcaff5
Ferenc Zachar <ferenczachar@gmail.com>
2023-11-17 17:42:19 +0100

    registration with pw hash done + login started

---
+++ public/login.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('input');
+let username = document.querySelector('#username');
+let password = document.querySelector('#password');
+const errorMsg = document.querySelector('#error');
+
+
+form.addEventListener('submit', async (e) => {
+    if (username.value === '' || password.value === ''){
+        e.preventDefault();
+        errorMsg.innerHTML = "Please enter both username and password";
+        form.reset();
+    }
+});
